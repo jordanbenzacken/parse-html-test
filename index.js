2790a2e94deb9657fb486bd5d857c4d59a802602
jordanbenzacken <jbzkjbzk@gmail.com>
2018-10-06 19:41:01 +0200

    read and write ok - compute status - error management

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const { promisify } = require('util')
+
+//transform callback in promise
+const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile)
+const result = {}
+
+const onError = (e, functionName) => {
+    result['status'] = 'error : ' + e
+    console.error(functionName + ' : ' + e)
+}
+
+const processHtmlTest = async (data) => {
+    try {
+        result['status'] = 'ok'
+        return result
+    }
+    catch (e) {
+        onError(e, 'processHtmlTest')
+    }
+}
+
+const readTestHtml = async () => {
+    try {
+        return await readFile('./test.html', 'utf-8')
+    }
+    catch (e) {
+        onError(e, 'readTestHtml')
+    }
+}
+
+const writeJsonResult = async (dataString) => {
+    try {
+        await writeFile('./my-result.json', dataString)
+    }
+    catch (e) {
+        onError(e, 'writeJsonResult')
+    }
+}
+
+const app = async () => {
+    const testHtml = await readTestHtml()
+    const dataParsed = await processHtmlTest(testHtml)
+    await writeJsonResult(JSON.stringify(dataParsed, null, 3))
+}
+
+app()
