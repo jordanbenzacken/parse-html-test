a04c9761ee385228c4679435f2f806314fe5e8e1
jordanbenzacken <jbzkjbzk@gmail.com>
2018-10-07 13:37:27 +0200

    processing in progress -

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const { promisify } = require('util')
 const cheerio = require('cheerio')
+const url = require('url')
+const queryString = require('query-string');
+
 
 //transform callback in promise
 const writeFile = promisify(fs.writeFile)
@@ -12,11 +15,57 @@ const onError = (e, functionName) => {
     console.error(functionName + ' : ' + e)
 }
 
+const getMainInfos = ($) => {
+    const firstPrimaryLink = $('.primary-link').first()
+    const urlPrimary = firstPrimaryLink.attr('href')
+    const queryData = url.parse(urlPrimary).query
+    const queryParsed = queryString.parse(queryData)
+    return { name: queryParsed.ownerName, code: queryParsed.pnrRef }
+}
+
+const tryToGet = (element, selector) => {
+    if (element.hasClass(selector)) {
+        return element
+    }
+    const target = element.find('.' + selector)
+    if (target.length !== 0) {
+        return target
+    }
+    return false
+}
+
+const getRoundTrips = ($) => {
+    const firstProductHeader = $('.product-header').first()
+    let roundTrips = []
+    firstProductHeader.siblings().map((i, sibling) => {
+        const element = $(sibling)
+        let target = false
+        target = tryToGet(element, 'product-travel-date')
+        if (target) {
+            roundTrips.push({})
+        }
+        target = tryToGet(element, 'product-details')
+        if (target) {
+            roundTrips[roundTrips.length - 1]['type'] = target.find('.travel-way').html().trim()
+            const train = {}
+            train['departureTime'] = target.find('.origin-destination-hour').html().replace('h', ':').trim()
+            train['departureStation'] = target.find('.origin-destination-station').html().trim()
+            roundTrips[roundTrips.length - 1]['trains'] = [train]
+            debugger
+        }
+    })
+    return roundTrips
+}
+
 const processHtmlTest = async (data) => {
     try {
-        const result = {}
-        const $ = cheerio.load(data)
-        return result
+        dataNoEscape = data.replace(/\\"/g, '"')
+        const $ = cheerio.load(dataNoEscape)
+        const { name, code } = getMainInfos($)
+        const roundTrips = getRoundTrips($)
+        const details = { roundTrips }
+        const result = { trips: [{ code, name, details }] }
+        return { result }
     }
     catch (e) {
         onError(e, 'processHtmlTest')
@@ -49,7 +98,7 @@ const app = async () => {
     } catch (e) {
         console.error(e)
     }
-    await writeJsonResult(JSON.stringify({ status, ...dataParsed }, null, 3))
+    await writeJsonResult(JSON.stringify({ status, ...dataParsed }, null, 2))
 }
 
 app()
